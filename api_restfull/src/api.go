954d5d2cc98b3feef3207a8448b418dcf66c8d73
Matheus Santos <matheus.santos@nappsolutions.com>
2024-09-19 21:19:29 -0300

    [FEAT] Refatorando main

--- api_restfull/src/api.go
+++ api_restfull/src/api.go
@@ -1,60 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"api_restfull/src/controllers"
 )
-
-type Customer struct {
-	Name string `json:"name"`
-	Idade int `json:"idade"`
-	Endereco string `json:"endereco"`
-	Products []Products `json:"products"`
-}
-
-type Products struct {
-	ProductName string `json:"product_name"`
-	ProductPrice string `json:"product_price"`
-	ProductCode string `json:"product_code"`
-}
-
-var Customers []Customer
-
-func HandleReadCustomers(w http.ResponseWriter, r *http.Request) {
-
-	log.Print("Read Customers")
-	
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Customers)
-
-}
-
-func HandleCreateCustomer(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-
-	var customer Customer
-
-	err := json.NewDecoder(r.Body).Decode(&customer)
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	log.Print("Create Customer: ", customer)
-	Customers = append(Customers, customer)
-
-	json.NewEncoder(w).Encode(Customers)
-
-}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/Customers", HandleReadCustomers).Methods("GET")
-	r.HandleFunc("/Customers", HandleCreateCustomer).Methods("POST")
+
+	// custumers routes
+	r.HandleFunc("/Customers", controllers.HandleReadCustomers).Methods("GET")
+	r.HandleFunc("/Customers", controllers.HandleCreateCustomer).Methods("POST")
+
+	// products routes
+	r.HandleFunc("/Products", controllers.HandleReadProducts).Methods("GET")
+	r.HandleFunc("/Products", controllers.HandleCreateProducts).Methods("POST")
+
+	// sales routes
+	r.HandleFunc("/Sales", controllers.HandleReadSale).Methods("GET")
+	r.HandleFunc("/Sales", controllers.HandleCreateSale).Methods("POST")
 
 	http.ListenAndServe(":8090", r)
 }
